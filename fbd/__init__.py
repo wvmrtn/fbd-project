347b9ad68f39cc37fc3b227c57fbe790d70444a5
wvmrtn <william.vincent.martin@gmail.com>
2020-12-11 16:41:55 +0100

    initial commit

---
+++ fbd/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Created on Fri Dec 11 16:40:39 2020.
+
+@author: williammartin
+"""
+
+# import standard libraries
+# import third-party libraries
+# import local libraries
+
+if __name__ == '__main__':
+    pass
